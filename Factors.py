c8a4a9f6ccef4cf3bf1879b219f943e00a49c63f
AsifQamar <93258479+AsifQamar@users.noreply.github.com>
2021-10-27 16:25:44 +0530

    Add files via upload

---
+++ Factors.py
@@ -0,0 +1,8 @@
+num = int(input("Enter a number of whose Factor you want: "))
+def print_factors(x):
+   print("The factors of",x,"are:")
+   for i in range(1, x + 1):
+       if x % i == 0:
+           print(i)
+
+print_factors(num)
